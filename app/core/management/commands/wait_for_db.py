425d002ee9a1b5e65ce98a88c1ad12a5041bea4b
al1168 <business.aldenlu@gmail.com>
2024-07-31 17:22:42 -0400

    added wait for db unit test

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+""" 
+Django command ot wait for the dartabsae to abe available 
+"""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+
+    def handle(self,*args,**options):
+        pass
