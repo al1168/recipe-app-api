6b40429608bf957b81e5ebd6279c0a69e5161f6a
al1168 <business.aldenlu@gmail.com>
2024-09-25 21:50:21 -0400

    added filtering on serializer

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 
+
 from rest_framework.test import APIClient
 from rest_framework import status
 
--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -368,6 +368,53 @@ class PrivateRecipeAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertIn(ingredient, recipe.ingredients.all())
 
+    def test_filter_by_tags(self):
+        """Test filtering recipes by tags"""
+        r1 = create_recipe(user=self.user, title='Thai Vegetable Curry')
+        r2 = create_recipe(user=self.user, title='Aubergine with Tahnini')
+
+        tag1 = Tag.objects.create(user=self.user, name='Vegan')
+        tag2 = Tag.objects.create(user=self.user, name='Vegetarian')
+
+        r1.tags.add(tag1)
+        r2.tags.add(tag2)
+        r3 = create_recipe(user=self.user, title='Fish and Chips')
+
+        params = {'tags': f'{tag1.id},{tag2.id}'}
+        res = self.client.get(RECIPE_URL, params)
+
+        s1 = RecipeSerializer(r1)
+        s2 = RecipeSerializer(r2)
+        s3 = RecipeSerializer(r3)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
+    def test_filter_by_ingredients(self):  # need to add back test
+        """Test filter recipes by ingredients"""
+        ingredient_1 = Ingredient.objects.create(user=self.user, name="mango")
+        ingredient_2 = Ingredient.objects.create(user=self.user, name="apple")
+
+        r1 = create_recipe(user=self.user, title='cool mango')
+        r2 = create_recipe(user=self.user, title="Def not mangos")
+        r3 = create_recipe(user=self.user, title="apple mango")
+        params = {'ingredients': f'{ingredient_1.id},{ingredient_2.id}'}
+        r1.ingredients.add(ingredient_1)
+        r3.ingredients.add(ingredient_1)
+        r3.ingredients.add(ingredient_2)
+
+        res = self.client.get(RECIPE_URL, params)
+        s1 = RecipeSerializer(r1)
+        s2 = RecipeSerializer(r2)
+        s3 = RecipeSerializer(r3)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s3.data, res.data)
+        self.assertNotIn(s2.data, res.data)
+
 
 class ImageUploadTests(TestCase):
 
