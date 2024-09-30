5b1b5541d1a9813b78c2955c9bbbcc10eca817fd
al1168 <business.aldenlu@gmail.com>
2024-09-30 00:13:30 -0400

    troubleshoot

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,4 +8,4 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi --buffer-size=32768
